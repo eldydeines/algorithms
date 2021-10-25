c9c243b42e2d3bf1c90fb71d41146518837dbe52
Eldy Deines <eldy.deines@outlook.com>
2021-10-25 11:44:51 -0600

    finished graphs exercises

--- graphs/graph.js
+++ graphs/graph.js
@@ -11,25 +11,74 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+    return this;
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.addVertex(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+    return this;
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    for (let node in this.nodes) {
+      node.adjacent = node.adjacent.filter(n => n !== vertex.value);
+    }
+    this.nodes.delete(vertex);
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    let stack = [start];
+    let seen = new Set(stack);
+    let seenArr = [start.value]
+    while (stack.length > 0) {
+      let currentVertex = stack.pop();
+      for (let neighbor of currentVertex.adjacent) {
+        if (!seen.has(neighbor)) {
+          stack.push(neighbor);
+          seen.add(neighbor);
+          seenArr.push(neighbor.value);
+        }
+      }
+    }
+    return seenArr;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    let stack = [start];
+    let seen = new Set(stack);
+    let seenArr = [start.value]
+    while (stack.length > 0) {
+      let currentVertex = stack.shift();
+      for (let neighbor of currentVertex.adjacent) {
+        if (!seen.has(neighbor)) {
+          stack.push(neighbor);
+          seen.add(neighbor);
+          seenArr.push(neighbor.value);
+        }
+      }
+    }
+    return seenArr;
+  }
 }
 
-module.exports = {Graph, Node}
+module.exports = { Graph, Node }
